b1e3292415212bc66543be475d7683e8b281ade9
Fynn Mahringer <fynn.mahringer@outlook.com>
2024-11-28 10:56:17 +0100

    Add spacing in AdventCalendar.java for readability
    Inserted blank lines in the AdventCalendar.java file to improve code readability and prevent potential merge conflicts. This change does not affect functionality.

--- src/main/java/htl/steyr/adventcalender/AdventCalendar.java
+++ src/main/java/htl/steyr/adventcalender/AdventCalendar.java
@@ -14,6 +14,8 @@ import javafx.stage.Stage;
 
 public class AdventCalendar extends Application {
 
+
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Advent Calendar");
