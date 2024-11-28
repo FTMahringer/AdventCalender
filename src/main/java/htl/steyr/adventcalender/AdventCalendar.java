061399ffde1d19c44649a62a793c0fed4566f8f0
Fynn Mahringer <fynn.mahringer@outlook.com>
2024-11-28 10:52:01 +0100

    Update Maven build command and remove redundant newline
    Switched Maven goal from javafx:jlink to package to correct the build process. Also removed an unnecessary newline in AdventCalendar.java to improve code readability.

--- src/main/java/htl/steyr/adventcalender/AdventCalendar.java
+++ src/main/java/htl/steyr/adventcalender/AdventCalendar.java
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
 
 public class AdventCalendar extends Application {
 
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Advent Calendar");
